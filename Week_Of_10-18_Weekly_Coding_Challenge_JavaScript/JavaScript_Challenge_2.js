66c7c332b96b2f755611b89fa679729883a86c5d
DouglasRamirez1 <douglasram4018@gmail.com>
2021-10-21 16:02:55 -0500

    cleanup

--- Week_Of_10-18_Weekly_Coding_Challenge_JavaScript/JavaScript_Challenge_2.js
+++ Week_Of_10-18_Weekly_Coding_Challenge_JavaScript/JavaScript_Challenge_2.js
@@ -17,10 +17,14 @@ function doesTriangleFit(triangle1 = [], triangle2 = []){
 
             //Returns the boolean result of whether the first triangle can fit in the second triangle or not.
             for(let i = 0; i < 3; i++){
+
+                //If all indexes of the first array are smaller than the second, then it fits. Otherwise it rotates until it reaches the end of the loop and returns false.
                 if((triangle1[i%3] <= triangle2[0]) && (triangle1[(i+1)%3] <= triangle2[1]) && (triangle1[(i+2)%3] <= triangle2[2])){
                     return true;
                 }
             }
+            //Returns false if both triangles had valid dimensions but triangle one could not fit into triangle two in any rotation.
+            return false;
         }
         else{
             //Returns false if the second triangle has invalid dimentions.
